13579bb2b97c3ff972e4201656c0b90287818afa
Sam <samuel.fish@live.com>
2024-10-26 19:52:34 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ rm -f "$HOME/.cryo_utilities/cryo_utilities" &>/dev/null
 # Attempt to download the binary 3 times.
 for i in {1..3}; do
   # Download binary
-  wget https://github.com/CryoByte33/steam-deck-utilities/releases/download/latest/cryo_utilities -O "$HOME/.cryo_utilities/cryo_utilities" 2>&1 | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Downloading at \2\/s, ETA \3/' | zenity --progress --title="Downloading CU Binary, attempt $i of 3..." --auto-close --width=500
+  wget https://github.com/FishPhd/steam-deck-utilities/releases/download/latest/cryo_utilities -O "$HOME/.cryo_utilities/cryo_utilities" 2>&1 | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Downloading at \2\/s, ETA \3/' | zenity --progress --title="Downloading CU Binary, attempt $i of 3..." --auto-close --width=500
 
   # Start a loop testing if zenity is running, and if not kill wget (allows for cancel to work)
   RUNNING=0
